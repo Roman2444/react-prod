0a6d0703805282014ba91d5c3f6c127de84e7224
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2026-08-06 19:08:20 +0300

    test huski pre-commit, add extentions and settings to vscode

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard-scss"],
-    "rules": {
-    "selector-class-pattern": null
-  }
+    extends: ['stylelint-config-standard-scss'],
+    rules: {
+        'selector-class-pattern': null,
+    },
 };
