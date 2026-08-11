7b6e3d0cd39a211b3a104a8638513e088336ca0a
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2026-08-11 18:03:48 +0300

    add getSidebarItems

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -3,7 +3,6 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ProfileCard } from 'entities/Profile';
 import { Country } from 'entities/Country';
 import { Currency } from 'entities/Currency';
-import avatar from 'shared/assets/tests/storybook.jpg';
 
 export default {
     title: 'entities/ProfileCard',
@@ -25,7 +24,7 @@ Primary.args = {
         first: 'asd',
         city: 'asf',
         currency: Currency.USD,
-        avatar,
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR66Z47_AmyUVUsczzKcrGsGE-AcfNhq_WqaFEy5y89ZQ&s',
     },
 };
 
